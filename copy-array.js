62b8a062932017af6442648a21db8571e411d5bc
guilherme <guilhermesilvas2004@gmail.com>
2024-03-07 14:58:40 -0300

    Clonando com spread operator

---
+++ copy-array.js
@@ -0,0 +1,9 @@
+const notas = [7, 7, 8, 9]
+
+const novasNotas = [...notas, 0]
+
+
+novasNotas.push(10)
+
+console.log(`As novas notas são ${novasNotas}`)
+console.log(`as notas originais são ${notas}`)
